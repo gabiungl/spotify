150f7c2a4154b5239b9e35a01eb3f7407b50a60b
Gabriela <gabiungarette@gmail.com>
2026-05-19 14:38:00 -0300

    first commit

---
+++ lib/home-page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: const Center(
+        child: Text(
+          'Home Screen',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'splash-screen.dart';
+
+void main() {
+  runApp(const SpotifyApp());
+}
+
+class SpotifyApp extends StatelessWidget {
+  const SpotifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
